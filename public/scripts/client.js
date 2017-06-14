4a79dd07034ef0b0a07fe86f66437b95a4e502d6
Peter Wildberger <pete.wildberger@gmail.com>
2017-06-14 14:41:38 -0500

    cleanup

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -20,10 +20,6 @@ function GiphyActivity($http) {
     //   });//end http
   }; // startup
   vm.getRando = function() {
-    // var searchURL = 'http://api.giphy.com/v1/gifs/search?q=';
-    // searchURL += 'random';
-    // searchURL += '&api_key=dc6zaTOxFJmzC';
-    // console.log('random GET');
     $http.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=').then(function(response) {
       console.log('back with: ', response);
       vm.searchResult = response.data.data;
