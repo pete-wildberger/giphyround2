5a8e4228e18682cf47976da58b244fec8e9bf486
Peter Wildberger <pete.wildberger@gmail.com>
2017-06-15 10:38:50 -0500

    clean up+remove button works

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,30 +8,16 @@ function GiphyActivity(GetService) {
   var vm = this;
   console.log('controller is loaded');
   vm.showRando = false;
-  vm.onReady = function() {
+  vm.searchResult = [];
 
-    //   console.log('startup');
-    //   //get call to '/songs/'
-    //   $http({
-    //     method: 'GET',
-    //     url: '/songs'
-    //   }).then(function(response) {
-    //     console.log('back with', response);
-    //   });//end http
-  }; // startup
   vm.displayRando = function() {
-    vm.showRando = false;
+    vm.showRando = true;
+      vm.searchResult = [];
     GetService.getRando().then(function(response) {
       console.log('back with: ', response);
-      vm.searchResult = response;
-      toggleShow();
+      vm.searchResult.push(response);
+
     });
-  };
-
-
-  var toggleShow = function() {
-    vm.showRando = !vm.showRando;
-    console.log(vm.showRando);
   };
 
   vm.getGifs = function() {
@@ -41,11 +27,10 @@ function GiphyActivity(GetService) {
     GetService.searchGif(search).then(function(res) {
       console.log(res);
       vm.searchResult = res;
-
     });
+
     vm.removeRow = function(index) {
       vm.searchResult.splice(index, 1);
     };
-
   };
 } //end controller function
