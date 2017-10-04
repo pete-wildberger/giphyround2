35d9ba5d4618415da348344e9078f278de80b183
Peter Wildberger <pete.wildberger@gmail.com>
2017-10-04 13:11:38 -0500

    service

--- public/scripts/services/get.service.js
+++ public/scripts/services/get.service.js
@@ -2,9 +2,9 @@ app.service('GetService', function($http) {
 
 
   this.getRando = function() {
+    //make $http call to giphy
     return $http.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=').then(function(response) {
       return response.data.data;
-
     }); //end get
   }; //end getRando
 
@@ -12,11 +12,11 @@ app.service('GetService', function($http) {
     var searchURL = 'http://api.giphy.com/v1/gifs/search?q=';
     searchURL += url;
     searchURL += '&api_key=dc6zaTOxFJmzC';
+    //make $http call to giphy with query
     return $http.get(searchURL).then(function(response) {
       console.log('back with: ', response);
       return response.data.data;
+    });//end get
+  };// end searchGif
 
-    });
-  };
-
-});
+});// end service
